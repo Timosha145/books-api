380e7d58b0f0592a54f53f45f927f18737a1c9ba
Your Name <you@example.com>
2023-10-13 11:05:22 +0300

    As an app developer I can get server response (200, 404) while searching a book by ID [Delivers #186246617]

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
 const app = require('express')()
 const port = 8080
 const swaggerUi = require('swagger-ui-express')
-const swaggerDocument = require('./docs/swagger.json');
+const yamls = require('yamljs');
+const swaggerDocument = yamls.load('./docs/swagger.yaml');
+
+const books = [
+    { id: 1, title: "To Kill a Mockingbird", price: 12.99 },
+    { id: 2, title: "1984", price: 10.99 },
+    { id: 3, title: "Pride and Prejudice", price: 9.99 },
+    { id: 4, title: "The Great Gatsby", price: 11.49 },
+    { id: 5, title: "The Catcher in the Rye", price: 10.00 },
+    { id: 6, title: "Moby-Dick", price: 14.99 },
+    { id: 7, title: "War and Peace", price: 18.99 },
+    { id: 8, title: "The Hobbit", price: 12.99 },
+    { id: 9, title: "Brave New World", price: 11.99 },
+    { id: 10, title: "The Lord of the Rings", price: 20.50 }
+];
 
 app.get('/books', (req, res) => {
-    const books = ["Eesti Keele Õpik", "Vene Keele Õpik"];
-    res.json(books);
+    res.send(books);
 });
+
+app.get('/books/:id', (req, res) => {
+
+    if (typeof books[req.params.id - 1] === 'undefined') {
+    return res.status(404).send({error: "Book not found"})
+    }
+
+    res.send(books[req.params.id - 1])
+})
 
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
