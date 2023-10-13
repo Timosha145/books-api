3b884e261aa60b4d45bf42863bfebc825cd822eb
Your Name <you@example.com>
2023-10-13 12:11:24 +0300

    Able to add books [Delivers #186246622]

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-const app = require('express')()
+const express = require('express');
+const app = express()
 const port = 8080
 const swaggerUi = require('swagger-ui-express')
 const yamls = require('yamljs');
 const swaggerDocument = yamls.load('./docs/swagger.yaml');
+
+app.use(express.json())
 
 const books = [
     { id: 1, title: "To Kill a Mockingbird", price: 12.99 },
@@ -22,16 +25,39 @@ app.get('/books', (req, res) => {
 });
 
 app.get('/books/:id', (req, res) => {
-
     if (typeof books[req.params.id - 1] === 'undefined') {
-    return res.status(404).send({error: "Book not found"})
+        return res.status(404).send({ error: "Book not found" });
+    }
+    res.send(books[req.params.id - 1]);
+});
+
+app.post('/books', (req, res) => {
+    if (!req.body.title || !req.body.price) {
+        return res.status(400).send({ error: 'One or all params are missing' });
     }
 
-    res.send(books[req.params.id - 1])
-})
+    let book = {
+        id: books.length + 1,
+        title: req.body.title,
+        price: req.body.price
+    };
+
+    books.push(book);
+
+    res.status(201)
+        .location(`${getBaseUrl(req)}/books/${books.length}`)
+        .send(book);
+});
+
+
 
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(port, () => {
-    console.log(`API up at: http://localhost:${port}`)
-})
+    console.log(`API up at: http://localhost:${port}`);
+});
+
+function getBaseUrl(req) {
+    return req.connection && req.connection.encrypted
+        ? 'https' : 'http' + `://${req.headers.host}`;
+}
