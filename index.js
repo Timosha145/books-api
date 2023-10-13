7f745b5c6cf35fac8c403c93dfc9ccf8b44a1055
Your Name <you@example.com>
2023-10-13 10:32:49 +0300

    As an app developer I can get a list of all games to show in my app [Delivers #186246575]

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ const port = 8080
 const swaggerUi = require('swagger-ui-express')
 const swaggerDocument = require('./docs/swagger.json');
 
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+app.get('/books', (req, res) => {
+    const books = ["Eesti Keele Õpik", "Vene Keele Õpik"];
+    res.json(books);
+});
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(port, () => {
     console.log(`API up at: http://localhost:${port}`)
